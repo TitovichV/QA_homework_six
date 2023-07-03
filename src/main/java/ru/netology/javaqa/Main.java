e48f57175bb6ec31e4040e9c9d14128a1844503e
vika_t <selga1991@yandex.ru>
2023-07-03 14:53:10 +0700

    initial commit

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,15 @@
+package ru.netology.javaqa;
+
+public class Main {
+    public static void main(String[] args) {
+        FreelanceServices services = new FreelanceServices();
+
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+
+        int month = FreelanceServices.calculate(income, expenses, threshold);
+
+        System.out.println("Месяцев отдыха: " + month);
+    }
+}
